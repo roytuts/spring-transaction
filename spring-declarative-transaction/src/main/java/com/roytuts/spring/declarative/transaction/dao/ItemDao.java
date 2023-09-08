adbddce00bd21c041a898bcf60085369444f9159
Soumitra <projwebinfo@gmail.com>
2023-09-08 13:00:40 +0530

    Update ItemDao.java

--- spring-declarative-transaction/src/main/java/com/roytuts/spring/declarative/transaction/dao/ItemDao.java
+++ spring-declarative-transaction/src/main/java/com/roytuts/spring/declarative/transaction/dao/ItemDao.java
@@ -13,32 +13,32 @@ import com.roytuts.spring.declarative.transaction.row.mapper.ItemRowMapper;
 @Repository
 public class ItemDao {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    public List<Item> getItems() {
-        String sql = "SELECT * FROM item";
-        List<Item> items = new ArrayList<>();
-        items = jdbcTemplate.query(sql, new ItemRowMapper());
-        return items;
-    }
+	public List<Item> getItems() {
+		String sql = "SELECT * FROM item";
+		List<Item> items = new ArrayList<>();
+		items = jdbcTemplate.query(sql, new ItemRowMapper());
+		return items;
+	}
 
-    public void addItem(Item item) {
-        String sql = "INSERT INTO items(item_id, item_name, item_desc, item_price) VALUES (?,?, ?, ?);";
-        jdbcTemplate.update(sql,
-                new Object[] { item.getItemId(), item.getItemName(), item.getItemDesc(), item.getItemPrice() });
-    }
+	public void addItem(Item item) {
+		String sql = "INSERT INTO item(item_id, item_name, item_desc, item_price) VALUES (?,?, ?, ?);";
+		jdbcTemplate.update(sql,
+				new Object[] { item.getItemId(), item.getItemName(), item.getItemDesc(), item.getItemPrice() });
+	}
 
-    public void updateItem(Item item) {
-        String sql = "UPDATE items SET item_name=?, item_desc=?, item_price=? WHERE item_id=?;";
-        jdbcTemplate.update(sql,
-                new Object[] { item.getItemName(), item.getItemDesc(), item.getItemPrice(), item.getItemId() });
-    }
+	public void updateItem(Item item) {
+		String sql = "UPDATE item SET item_name=?, item_desc=?, item_price=? WHERE item_id=?;";
+		jdbcTemplate.update(sql,
+				new Object[] { item.getItemName(), item.getItemDesc(), item.getItemPrice(), item.getItemId() });
+	}
 
-    public void deleteItem(Item item) throws Exception {
-        String sql = "DELETE FROM items WHERE item_id=?;";
-        jdbcTemplate.update(sql, new Object[] { item.getItemId() });
-        throw new Exception();
-    }
+	public void deleteItem(Item item) throws Exception {
+		String sql = "DELETE FROM item WHERE item_id=?;";
+		jdbcTemplate.update(sql, new Object[] { item.getItemId() });
+		throw new Exception();
+	}
 
 }
